faa41ce503c0421f74113bb677274662e1c60369
dieplh <dieplh.elite@gmail.com>
2021-01-24 16:13:13 +0900

    update axios

--- lib/axiosClient.ts
+++ lib/axiosClient.ts
@@ -15,6 +15,7 @@ axiosClient.interceptors.request.use(async (config) => {
 });
 axiosClient.interceptors.response.use(
   (response) => {
+    if (response && response.data) return response.data;
     return response;
   },
   (error) => {
