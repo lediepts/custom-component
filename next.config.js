4640695b078a1583573fd11c2e10e6d2b5bb26a1
dieplh <dieplh.elite@gmail.com>
2021-01-24 19:00:14 +0900

    update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,58 @@
+const fs = require("fs");
+const env = fs
+  .readFileSync("./.env", { encoding: "utf-8" })
+  .replace(/"|'|\r/gim, "")
+  .trim()
+  .split("\n")
+  .filter((str) => str && !str.includes("#"))
+  .map((v) => {
+    return v.replace("=", '":"');
+  })
+  .reduce((next, cur) => {
+    return Object.assign(next, JSON.parse('{"' + cur + '"}'));
+  }, {});
+
 module.exports = {
-  env: getENV(process.env),
+  env: env,
 };
 
-function getENV(envObj) {
-  let rs = {};
-  Object.keys(envObj).map((key, _i) => {
-    if (key.includes(envObj["ENV_KEY"])) return (rs[key] = envObj[key]);
-  });
-  return rs
-}
+// function getENV(envObj) {
+//   let rs = {};
+//   console.log(envObj);
+//   Object.keys(envObj).map((key, _i) => {
+//     if (key.includes(envObj["ENV_KEY"])) return (rs[key] = envObj[key]);
+//   });
+//   return rs;
+// }
+
+// const fs = require("fs");
+// const data = fs
+//   .readFileSync("./.env", { encoding: "utf-8" })
+//   .replace(/"|'|\r/gim, "")
+//   .trim()
+//   .split("\n")
+//   .filter((str) => str && !str.includes("#"))
+//   .map((v) => {
+//     return v.replace("=", '":"');
+//   })
+//   .reduce((next, cur) => {
+//     return Object.assign(next, JSON.parse('{"' + cur + '"}'));
+//   }, {});
+// console.log(data)
+// async function loadENV() {
+//   fs.readFile("./.env", "utf8", function (err, data) {
+//     if (err) throw err;
+//     let obj = data
+//       .replace(/\r/gim, "")
+//       .trim()
+//       .split("\n")
+//       .filter((str) => str && !str.includes("#"))
+//       .map((v) => {
+//         return v.replace("=", '":"');
+//       })
+//       .reduce((next, cur) => {
+//         return Object.assign(next, JSON.parse('{"' + cur + '"}'));
+//       }, {});
+//     return obj;
+//   });
+// }
